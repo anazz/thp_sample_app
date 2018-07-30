9a6bf7c4d58a0674c250fef6dc1c7377207e8099
Ana Zegheanu <ana.zegheanu@gmail.com>
2018-07-30 18:33:16 +0200

    account activation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'sessions/new'
+
   get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
@@ -12,5 +13,7 @@ Rails.application.routes.draw do
   get  '/login',      to: 'sessions#new'
   post '/login',      to: 'sessions#create'
   delete '/logout',   to: 'sessions#delete'
+
+  resources :account_activations, only: [:edit]
 
 end
