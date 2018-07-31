fa7d5b6b59d7880cd0eeab50a16c0212dbcdcf07
Ana Zegheanu <ana.zegheanu@gmail.com>
2018-07-31 17:23:46 +0200

    password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
 
   get 'users/new'
@@ -15,5 +17,6 @@ Rails.application.routes.draw do
   delete '/logout',   to: 'sessions#delete'
 
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
 end
