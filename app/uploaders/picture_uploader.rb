9d100d1b0ee0e118fadf0e30bfee5c7eb5fc4ac9
Ana Zegheanu <ana.zegheanu@gmail.com>
2018-08-08 18:10:05 +0200

    Solving the image in production issue

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,7 +2,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
-  process resize_to_fit: [400, 400]
+  process resize_to_limit: [400, 400]
 
   if Rails.env.production?
     storage :fog
@@ -10,14 +10,15 @@ class PictureUploader < CarrierWave::Uploader::Base
     storage :file
   end
 
-  # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
-
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  # Add a white list of extensions which are allowed to be uploaded.
+  def extension_whitelist
+    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
